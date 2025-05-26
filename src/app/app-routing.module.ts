2a7dcf759a3e69495d12110d2dea9cfe549286d5
Vignesh <balarathnavignesh@particleblack.com>
2025-05-26 13:20:31 +0530

    card changes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './pages/gaurds/auth.gaurd';
 import { AppComponent } from './app.component';
+import { CardComponent } from './pages/digicard/card/card.component';
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent,
+  {
+    path: '', component: AppComponent,
     children: [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'pages', loadChildren: () => import(`./pages/pages.module`).then(m => m.PagesModule) },
       { path: 'login', loadChildren: () => import(`./login/login.module`).then(m => m.LoginModule) },
+      { path: 'card/:id', component: CardComponent },
+
     ]
   }
 
